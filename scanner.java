fc35d8c333e6f8f9c06b7f1e4a32cc46fd05bf07
AyaMHassan <54334085+AyaMHassan@users.noreply.github.com>
2022-11-26 23:42:45 +0200

    convert JAVAFX project to a reguler java project

--- scanner.java
+++ scanner.java
@@ -1,57 +1,33 @@
-package com.example.scanner;
+package com.company;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
-public class Scanner extends Application {
-    public String[] scanner(String s){
-        String[]arr={String.valueOf(0)};
+public class Scanner {
+    public String[] scanner(String s) {
+        String[] arr = {String.valueOf(0)};
+        String program;
         int len = 0;
         String state = "START";
         String token = "";
         String prevState = "";
-        for (int i = 0; i < len; i++){
-            if(state == "START"){
+        for (int i = 0; i < len; i++) {
+            if (state == "START") {
 
-            }
-            else if(state == "INRESERVEDWORDS"){
+            } else if (state == "INRESERVEDWORDS") {
 
-            }
-            else if(state == "INNUM"){
+            } else if (state == "INNUM") {
 
-            }
-            else if(state == "INID"){
+            } else if (state == "INID") {
 
-            }
-            else if(state == "INASSIGN"){
+            } else if (state == "INASSIGN") {
 
-            }
-            else if(state == "INSYMBOL"){
+            } else if (state == "INSYMBOL") {
 
-            }
-            else if(state == "DONE"){
+            } else if (state == "DONE") {
 
-            }
-            else if(state == "INCOMMENT"){
+            } else if (state == "INCOMMENT") {
 
             }
         }
         return arr;
     }
-
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Scanner.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
-    }
 }
+
