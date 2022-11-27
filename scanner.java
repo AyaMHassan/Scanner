406ffee99ce0cf961544a01ffb93cdde306a5acd
bassant1999 <72526468+bassant1999@users.noreply.github.com>
2022-11-27 19:17:11 +0200

    Update scanner.java

--- scanner.java
+++ scanner.java
@@ -9,7 +9,7 @@ import java.io.FileNotFoundException;  // Import this class to handle errors
 public class Scanner {
      public static void main(String[] args) throws IOException {
         // TODO code application logic here
-       String s = read("D:\\Old data 26-11-2020\\Documents\\bas\\bassantmah.txt");
+       String s = read("D:\\Old data 26-11-2020\\Documents\\bas\\example.txt");
        ArrayList<String> a=scanner(s);
        for (int i = 0; i < a.size() ; i++)
        {
@@ -81,7 +81,7 @@ public class Scanner {
                     if(i + 2 <= len) {
                         if (program.substring(i, i + 2).equals("if")) {
                                 token += "if";
-                                if (program.charAt(i + 2) == '\n' || program.charAt(i + 2) == ' ' || program.charAt(i + 2) == '\t' || program.charAt(i + 2) == '(') {
+                                if (program.charAt(i + 2) == '\n' || program.charAt(i + 2) == ' ' || program.charAt(i + 2) == '\t') {
                                     state = "DONE";
                                     type = substate;
                                     i = i + 2;
@@ -122,7 +122,7 @@ public class Scanner {
                             if(i+3 == len){
                                 state = "DONE";
                                 type = substate;
-                                i = i + 3;
+                                i = i + 1;
                             }
                             else if (program.charAt(i + 3) == '\n' || program.charAt(i + 3) == ' ' || program.charAt(i + 3) == '\t') {
                                 state = "DONE";
@@ -178,7 +178,7 @@ public class Scanner {
                     if(i+5 < len){
                         if(program.substring(i,i+5).equals("until")){
                             token+="until";
-                            if(program.charAt(i+5) == '\n' || program.charAt(i+5) == ' ' || program.charAt(i+5) == '\t' ||  program.charAt(i+5) == '('){
+                            if(program.charAt(i+5) == '\n' || program.charAt(i+5) == ' ' || program.charAt(i+5) == '\t'){
                                 state = "DONE";
                                 type = substate;
                                 i = i + 5;
@@ -198,7 +198,7 @@ public class Scanner {
                     if(i+5 < len){
                         if(program.substring(i,i+5).equals("write")){
                             token+="write";
-                            if(program.charAt(i+5) == '\n' || program.charAt(i+5) == ' ' || program.charAt(i+5) == '\t' || program.charAt(i+5) == '('){
+                            if(program.charAt(i+5) == '\n' || program.charAt(i+5) == ' ' || program.charAt(i+5) == '\t'){
                                 state = "DONE";
                                 type = substate;
                                 i = i + 5;
@@ -215,7 +215,26 @@ public class Scanner {
                     }
                 }
             }
-             else if (state == "INASSIGN") {
+             else if (state == "INNUM") {
+                if((program.charAt(i)>='0')&&(program.charAt(i)<='9')){
+                    token = token + program.charAt(i);
+                    type="NUMBER";
+                }
+                else {
+                    i = i - 1;
+                    state="DONE";
+                }
+            } else if (state == "INID") {
+                  if(((program.charAt(i)>='a')&&(program.charAt(i)<='z'))||((program.charAt(i)>='A')&&(program.charAt(i)<='Z'))){
+                    token+=program.charAt(i);
+                    type="Identifier";
+                }
+                else {
+                    i=i-1;
+                    state="DONE";
+                }
+
+            } else if (state == "INASSIGN") {
                 
                 if((program.charAt(i)==':') &&(program.charAt(i+1)=='=')){
                     i=i+1;
