8eff1d21ef32a1dfa091fad340ea7629e3ca3bd0
bassant1999 <72526468+bassant1999@users.noreply.github.com>
2022-11-27 18:46:16 +0200

    Update scanner.java

--- scanner.java
+++ scanner.java
@@ -1,22 +1,20 @@
 package com.company;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+
+
 public class Scanner {
-     public static void main(String[] args) {
+     public static void main(String[] args) throws IOException {
         // TODO code application logic here
-       String s="{ Sample program in TINY language –\n" +
-"computes factorial}\n" +
-"read x; {input an integer }\n" +
-"if 0 < x then { don’t compute if\n" +
-"x <= 0 }\n" +
-"fact := 1;\n" +
-"repeat\n" +
-"fact := fact * x;\n" +
-"x := x - 1\n" +
-"until x = 0;\n" +
-"write fact { output factorial of x\n" +
-"}\n" +
-"end";
+       String s = read("D:\\Old data 26-11-2020\\Documents\\bas\\bassantmah.txt");
        ArrayList<String> a=scanner(s);
+       for (int i = 0; i < a.size() ; i++)
+       {
+           System.out.print(a.get(i));
+       }
     }
      public static ArrayList<String> scanner(String s) {
         ArrayList<String> arr = new ArrayList<String>();
@@ -69,6 +67,11 @@ public class Scanner {
                     i = i - 1;
                 }else if(((program.charAt(i)>='a')&&(program.charAt(i)<='z'))||((program.charAt(i)>='A')&&(program.charAt(i)<='Z'))){
                     state="INID";
+                    i=i-1;
+                }
+                else if((program.charAt(i)>='0')&&(program.charAt(i)<='9'))
+                {
+                    state="INNUM";
                     i=i-1;
                 }
 
@@ -213,7 +216,14 @@ public class Scanner {
                 }
             }
              else if (state == "INNUM") {
-
+                if((program.charAt(i)>='0')&&(program.charAt(i)<='9')){
+                    token = token + program.charAt(i);
+                    type="NUMBER";
+                }
+                else {
+                    i = i - 1;
+                    state="DONE";
+                }
             } else if (state == "INID") {
                   if(((program.charAt(i)>='a')&&(program.charAt(i)<='z'))||((program.charAt(i)>='A')&&(program.charAt(i)<='Z'))){
                     token+=program.charAt(i);
@@ -297,5 +307,22 @@ public class Scanner {
         }
         return arr;
     }
+      public static String read(String path) throws FileNotFoundException, IOException {
+        String program = "";
+        // File path is passed as parameter
+        File file = new File(path);
+        // Creating an object of BufferedReader class
+        BufferedReader br
+            = new BufferedReader(new FileReader(file));
+ 
+        // Declaring a string variable
+        String st;
+        // Condition holds true till
+        // there is character in a string
+        while ((st = br.readLine()) != null)
+            program = program + st + '\n';
+//        System.out.println(program);
+        br.close();
+        return program;
+  }
 }
-
